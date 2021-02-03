82d30988dba4a517f2ce02340c54a8f011f47041
MoDo <modo@modo.lv>
2021-02-03 11:41:49 +0200

    cleanup_on_startup.bat works regardless of working dir.

--- batch/cleanup_on_startup.bat
+++ batch/cleanup_on_startup.bat
@@ -4,9 +4,9 @@ rem Rambox
 echo.
 echo # Emptying `%localappdata%\Temp`...
 echo.
-call empty_folder.bat "%localappdata%\Temp"
+call %~dp0\empty_folder.bat "%localappdata%\Temp"
 
 echo.
 echo # Clearing Rambox cache...
 echo.
-call remove_cache_folders.bat "%APPDATA%\Rambox\Partitions"
+call %~dp0\remove_cache_folders.bat "%APPDATA%\Rambox\Partitions"
